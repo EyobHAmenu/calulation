08be2183ebc6459f6b0152b9674339aadee34cdc
EyobHAmenu <heyob888@gmail.com>
2020-04-15 11:11:03 -0400

    Second commit

--- src/calculator.java
+++ src/calculator.java
@@ -7,10 +7,11 @@ public class calculator {
         int num2;
         int sum;
         int product;
+        String str1, str2;
         Scanner scan = new Scanner(System.in); // accept values from user
-        System.out.println("Enter a number");  // ask the user to enter number
+        System.out.print("Enter a number");  // ask the user to enter number
         num1 = scan.nextInt();                  // save the number entered from user on variable num1
-        System.out.println("Enter an other number");  // again ask the user to enter next number
+        System.out.print("Enter an other number");  // again ask the user to enter next number
         num2 = scan.nextInt();              // save the second number entered from user on the second variable num2
         sum = num1 + num2;              // save the addition result of num1 and num2 on variable sum
         product = num1 * num2;          // save teh product result of num1 and num 2 on variable product
@@ -18,5 +19,12 @@ public class calculator {
         System.out.println("Product: " + product); // print the product of the two numbers for the user
         System.out.println("Square of : " + num1 + " = " + num1*num1); // calculate and print the square of num1
         System.out.println("Square of : " + num2 + " = " + num2*num2); // calculate and print the square of num2
+
+        scan.nextLine();
+        System.out.println("Enter a word.");
+        str1 = scan.nextLine();
+        System.out.println("Enter another word.");
+        str2 = scan.nextLine();
+        System.out.println(str1 + str2);
     }
 }
